1e7fa44ccbb55e037087f5f562fdd04bcd4952ea
sagi <sagi@users.noreply.github.com>
2016-09-18 04:25:35 +0300

    hygiene

--- 2016/wieners-rsa-attack/quadratic.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-from sympy import *
-if __name__ == '__main__':
-    #p, N, e, k, di, phi = symbols('p N e k di phi', integer=True)
-    symbols('N phi ee k de', integer=True)
-    print(solve(p**2 + (phi - N - 1)*p + N, p))
-    
-
